11d796186b49e8855bfe4dbaf6089ccc74bc1264
evan yuanzh <yuanentai@yuanentaideiMac.local>
2020-01-09 16:52:24 +0800

    feat: 禁止tabnine在大项目中提示收费

--- evan/main-init.el
+++ evan/main-init.el
@@ -53,6 +53,16 @@
 
 (add-to-list 'company-transformers 'company//sort-by-tabnine t)
 
+;; The free version of TabNine is good enough,
+;; and below code is recommended that TabNine not always
+;; prompt me to purchase a paid version in a large project.
+(defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+  (let ((company-message-func (ad-get-arg 0)))
+    (when (and company-message-func
+               (stringp (funcall company-message-func)))
+      (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+        ad-do-it))))
+
 (company-tng-configure-default)
 (setq company-frontends
       '(company-tng-frontend
