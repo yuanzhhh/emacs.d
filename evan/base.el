fa0f7c885368375c2a22e90f57ef1bde3c0f6a71
yuanzh <yuanzh@yuanzhdeMacBook-Pro-4.local>
2020-02-03 02:35:39 +0800

    feat: conf

--- evan/base.el
+++ evan/base.el
@@ -235,10 +235,17 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts[x]?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
   (setq web-mode-content-types-alist
         '(("jsx" . ".*\\.js[x]?\\'"))))
+
+  (add-hook 'web-mode-hook
+      (lambda ()
+        ;; `:separate`  使得不同 backend 分开排序
+        (add-to-list 'company-backends '(company-tide :with company-tabnine :separate))
+        (setq web-mode-code-indent-offset 2)
+        (setq-local web-mode-enable-auto-quoting nil)))
 
 (defun setup-tide-mode ()
   (interactive)
@@ -250,39 +257,8 @@
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1))
 
-;; tide
-(use-package tide
-  :ensure t
-  :config
-  (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
-  ;; formats the buffer before saving
-  ;; (add-hook 'before-save-hook 'tide-format-before-save)
-  (add-hook 'web-mode-hook
-            (lambda ()
-              ;; JSX
-              (when (equal web-mode-content-type "jsx")
-                (setq-local emmet-expand-jsx-className? t)
-                (flycheck-add-mode 'javascript-eslint 'web-mode)
-                (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-                (setup-tide-mode))
-
-              ;; TSX
-              (when (string-equal "tsx" (file-name-extension buffer-file-name))
-                (setq-local emmet-expand-jsx-className? t)
-                (flycheck-add-mode 'typescript-tslint 'web-mode)
-                (flycheck-add-next-checker 'typescript-tslint 'jsx-tide 'append)
-                (setup-tide-mode))
-
-              ;; Typescript
-              (when (string-equal "ts" (file-name-extension buffer-file-name))
-                (flycheck-add-mode 'typescript-tslint 'web-mode)
-                (setup-tide-mode))
-
-              ;; `:separate`  使得不同 backend 分开排序
-              (add-to-list 'company-backends '(company-tide :with company-tabnine :separate))
-              (setq web-mode-code-indent-offset 2)
-              (setq-local web-mode-enable-auto-quoting nil))))
-
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
 
 ;; 当tide被加载后
 (with-eval-after-load 'tide
@@ -290,6 +266,31 @@
   (evil-define-key '(normal) tide-mode-map (kbd "g d") 'tide-jump-to-definition))
 
 (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
+
+(use-package tide :ensure t)
+
+(add-hook 'web-mode-hook
+  (lambda ()
+    (when (string-equal "tsx" (file-name-extension buffer-file-name))
+      (setq-local emmet-expand-jsx-className? t)
+      (flycheck-add-mode 'typescript-tslint 'web-mode)
+      (flycheck-add-next-checker 'typescript-tslint 'jsx-tide 'append))
+
+    (when (string-equal "ts" (file-name-extension buffer-file-name))
+      (flycheck-add-mode 'typescript-tslint 'web-mode))
+
+    (setup-tide-mode)
+    ))
+
+(flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+(add-hook 'web-mode-hook
+  (lambda ()
+    (when (equal web-mode-content-type "jsx")
+      (setq-local emmet-expand-jsx-className? t)
+      (flycheck-add-mode 'javascript-eslint 'web-mode)
+      (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+      (setup-tide-mode))
+    ))
 
 (require 'vue-mode)
 (require 'lsp-mode)
