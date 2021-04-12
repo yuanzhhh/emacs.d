dfddc63ee793e2915be6beecafc3b28176913c8a
jryuanentai <jryuanentai@jd.com>
2021-04-12 18:47:30 +0800

    feat(add): update

--- init.el
+++ init.el
@@ -54,6 +54,7 @@ values."
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '
    (
+    evil-collection
     org-download
     company
     company-tabnine
@@ -115,7 +116,7 @@ values."
    ;; (default t)
    dotspacemacs-elpa-https t
    ;; Maximum allowed time in seconds to contact an ELPA repository.
-   dotspacemacs-elpa-timeout 5
+   dotspacemacs-elpa-timeout 300
    ;; If non nil then spacemacs will check for updates at startup
    ;; when the current branch is not `develop'. Note that checking for
    ;; new versions works via git commands, thus it calls GitHub services
@@ -335,6 +336,10 @@ before packages are loaded. If you are unsure, you should try in setting them in
           ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
           ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
 
+  (setq evil-want-keybinding nil)
+  (when (require 'evil-collection nil t)
+    (evil-collection-init)
+    (require 'evil))
   ;; 设置中文字体
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
@@ -351,9 +356,11 @@ you should place your code here."
   (org-babel-load-file (expand-file-name "~/.spacemacs.d/evan/base.org" user-emacs-directory))
   (add-to-list 'load-path "~/.spacemacs.d/snails")
   (add-to-list 'load-path "~/.spacemacs.d/smart-align")
+  ;; (add-to-list 'load-path "~/.spacemacs.d/evil-magit")
   ;; (add-to-list 'load-path "~/.spacemacs.d/nerd-icons.el")
 
   ;; (require 'nerd-icons)
+  ;; (require 'evil-magit)
   ;; 弹框模糊搜索
   (require 'snails)
   ;; 赋值对齐
@@ -370,7 +377,7 @@ you should place your code here."
  '(evil-want-Y-yank-to-eol nil)
  '(package-selected-packages
    (quote
-    (valign centaur-tabs nadvice emacsql-sqlite3 prettier-js add-node-modules-path org-roam emacsql-sqlite emacsql deft shrink-path all-the-icons-ivy-rich doom-modeline color-theme-modern color-theme kaolin-themes pyim pyim-basedict xr pangu-spacing find-by-pinyin-dired ace-pinyin pinyinlib fcitx carbon-now-sh reveal-in-osx-finder company-tabnine posframe vc-msg git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter diff-hl yaml-mode vue-mode edit-indirect ssass-mode vue-html-mode nlinum multi-term lsp-mode ht dash-functional gruvbox-theme autothemer doom-themes unicode-escape names company autopair all-the-icons memoize ace-jump-mode web-mode web-beautify toml-mode tide typescript-mode flycheck tagedit smeargle slim-mode scss-mode sass-mode rainbow-mode rainbow-identifiers racer pos-tip pug-mode orgit nginx-mode mmm-mode markdown-toc magit-gitflow magit-popup lua-mode livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc helm-gitignore helm-css-scss haml-mode gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md evil-magit magit transient git-commit with-editor emmet-mode color-identifiers-mode coffee-mode cargo markdown-mode rust-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra lv hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile projectile pkg-info epl helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
+    (valign centaur-tabs nadvice emacsql-sqlite3 prettier-js add-node-modules-path org-roam emacsql-sqlite emacsql deft shrink-path all-the-icons-ivy-rich doom-modeline color-theme-modern color-theme kaolin-themes pyim pyim-basedict xr pangu-spacing find-by-pinyin-dired ace-pinyin pinyinlib fcitx carbon-now-sh reveal-in-osx-finder company-tabnine posframe vc-msg git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter diff-hl yaml-mode vue-mode edit-indirect ssass-mode vue-html-mode nlinum multi-term lsp-mode ht dash-functional gruvbox-theme autothemer doom-themes unicode-escape names company autopair all-the-icons memoize ace-jump-mode web-mode web-beautify toml-mode tide typescript-mode flycheck tagedit smeargle slim-mode scss-mode sass-mode rainbow-mode rainbow-identifiers racer pos-tip pug-mode orgit nginx-mode mmm-mode markdown-toc magit-gitflow magit-popup lua-mode livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc helm-gitignore helm-css-scss haml-mode gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md magit transient git-commit with-editor emmet-mode color-identifiers-mode coffee-mode cargo markdown-mode rust-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra lv hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile projectile pkg-info epl helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
  '(pyim-dicts
    (quote
     ((:name "def" :file "/Users/yuanentai/Desktop/pyim-bigdict.pyim")))))
